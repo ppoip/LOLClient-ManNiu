f2f52cef4e39e1cd60e6861bef26760d6c2ad43d
千羽結 <ppoip.c@gmail.com>
2017-04-20 22:07:15 +0800

    c

---
+++ LOLClient/Assets/Script/Fight/AnimStateConst.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimStateConst
+{
+    public const int IDLE = 0;
+    public const int RUN = 1;
+    public const int ATTACK = 2;
+    public const int SKILL = 3;
+    public const int DEATH = 4;
+}
---
+++ LOLClient/Assets/Script/Fight/PlayerController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class PlayerController : MonoBehaviour
+{
+    public FightDataModel data;
+
+    private NavMeshAgent navAgent;
+    private Animator animator;
+
+
+    private void Awake()
+    {
+        navAgent = GetComponent<NavMeshAgent>();
+        animator = GetComponent<Animator>();
+    }
+
+
+
+
+}
